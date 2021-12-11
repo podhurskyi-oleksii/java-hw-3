b2e89176e4cb6c778f6d1898f1bc4136acdf485a
podhurskyi-oleksii <35135825+podhurskyi-oleksii@users.noreply.github.com>
2021-12-12 01:23:17 +0200

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,18 @@
+package com.goit.testing;
+
+public class ATM {
+    public int countBanknotes(int sum) {
+
+        int[] nominal = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int count = 0;
+        int i = 0;
+        while(sum > 0) {
+            if (sum >= nominal[i]) {
+                count += sum / nominal[i];
+                sum %= nominal[i];
+            }
+            i++;
+        }
+        return count;
+    }
+}
